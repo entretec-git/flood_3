1ae64de5d10ab5ffd82adf02c0fa4360911dd38e
mike brown <developer@entretec.com>
2013-02-22 10:39:04 +0800

    Initial Commit

---
+++ Flood_3/floodViewController.h
@@ -0,0 +1,13 @@
+//
+//  floodViewController.h
+//  Flood_3
+//
+//  Created by mike on 22/02/13.
+//  Copyright (c) 2013 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface floodViewController : UIViewController
+
+@end
---
+++ Flood_3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flood_3
+//
+//  Created by mike on 22/02/13.
+//  Copyright (c) 2013 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "floodAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([floodAppDelegate class]));
+    }
+}
